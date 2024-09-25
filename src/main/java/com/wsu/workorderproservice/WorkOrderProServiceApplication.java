00e0abb9531a751f0e8da3ed0aa84f14f0ef87e0
cdanbury <113932920+cdanbury@users.noreply.github.com>
2024-09-25 16:29:27 -0400

    adding files for first assignment

---
+++ src/main/java/com/wsu/workorderproservice/WorkOrderProServiceApplication.java
@@ -0,0 +1,19 @@
+package com.wsu.workorderproservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+public class WorkOrderProServiceApplication {
+
+	/**
+	 * SpringBoot Starter method on embedded tomcat server
+	 * @param args - Allow to pass String array JVM arguments to set value dynamically during runtime
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(WorkOrderProServiceApplication.class, args);
+	}
+
+}
